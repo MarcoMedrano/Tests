dc43069c21f8e5cd9b22b0e6b1338be6bb618b2f
MarcoMedrano <marco.medrano.garay@gmail.com>
2016-03-24 11:56:49 -0400

    Added Hello World project.

---
+++ owin/HelloWorld/HelloWorld/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Owin;
+
+/// <summary>
+/// Summary description for Startup
+/// </summary>
+public class Startup
+{
+    public static void Configuration(IAppBuilder app)
+    {
+        app.Use(async (ctx, next) =>
+        {
+            Debug.WriteLine("Incoming request " + ctx.Request.Path);
+            await next();// calls the next middleware which actually writes hello world
+            Debug.WriteLine("Outgoing request " + ctx.Response.Body);
+        });
+        app.Use(async (ctx, next) => await ctx.Response.WriteAsync("<html><head></head><body>Hello world!</body></header>"));
+    }
+}
