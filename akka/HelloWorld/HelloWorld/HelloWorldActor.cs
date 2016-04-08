371a9d77b31bcc230829b010b1e49a0cfb525b1b
Marco Medrano <marco.medrano@ucn.net>
2016-04-08 17:17:14 -0400

    Added helloworld on akka.net

---
+++ akka/HelloWorld/HelloWorld/HelloWorldActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    using Akka.Actor;
+    class HelloWorldActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            switch (message.ToString())
+            {
+                case "Greet":
+                    Console.WriteLine("Hello World");
+                    break;
+                default:
+                    Console.WriteLine($"This actor {nameof(HelloWorldActor)} cant handle this message " + message);
+                    this.Unhandled(message);
+                    break;
+            }
+        }
+    }
+}
---
+++ akka/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    using Akka.Actor;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("HelloWorlActorSystem");
+            var helloActor = Props.Create<HelloWorldActor>();
+
+            var actorRef = actorSystem.ActorOf(helloActor, nameof(HelloWorldActor));
+            actorRef.Tell("Greet");
+            actorRef.Tell("!@#$");
+
+            Console.ReadKey();
+            actorSystem.Shutdown();
+        }
+    }
+}
