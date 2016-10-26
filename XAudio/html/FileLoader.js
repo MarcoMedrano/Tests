294f9dbffafadd6cd7fd7bc36731f7cc7ef54adc
Marco Medrano <marco.medrano.garay@gmail.com>
2016-10-26 11:24:44 -0400

    refactorized on smaller files.

---
+++ XAudio/html/FileLoader.js
@@ -0,0 +1,21 @@
+﻿function FileLoader() {
+    var self = this;
+
+    self.load = function (fileName, callback){
+
+        var request = new XMLHttpRequest();
+        request.open('GET', fileName, true);
+        request.responseType = 'arraybuffer';
+        
+        // Decode asynchronously
+        request.onload = function () {
+            var uint8Array = new Uint8Array(request.response);
+            console.info('received ' + uint8Array.length);
+
+             callback(uint8Array);
+        }
+
+        request.send();
+    };
+
+}
---
+++ XAudio/html/GsmDecoder.js
@@ -0,0 +1,26 @@
+﻿
+function GsmDecoder() {
+    var self = this;
+    
+    Module.ccall('Initialize');
+
+    self.decode = function(encodedBuffer) {
+
+        var decodedSamples = 3200;
+        var inputPtr = Module._malloc(encodedBuffer.length * encodedBuffer.BYTES_PER_ELEMENT);
+        Module.HEAPU8.set(encodedBuffer, inputPtr);
+            
+        var buf = Module._malloc(decodedSamples * Int16Array.BYTES_PER_ELEMENT);
+        var len2 = Module.ccall('decode_block', 'number', ['pointer', 'number', 'pointer'], [inputPtr, encodedBuffer.length, buf]);
+            
+        var decodedBuffer = new Int16Array(Module.HEAPU8.buffer.slice(buf, buf + len2));
+        var decodedFloat = new Float32Array(decodedSamples);
+            
+        for (var i = 0; i < decodedSamples; i++) {
+            decodedFloat[i] = (decodedBuffer[i] / 32768);
+        }
+            
+        //console.log("GsmDecoder: DecodeEncodedBlock: Size is " + deencodedBuffer.length);
+        return decodedFloat; // this should be 320 floats break point to ensure logic comes out this way, if not, something small above is wrong    
+    };
+}
