912317e080e3fa513eb97c92f73a85e16bd1fc1e
Marco Medrano <marco.medrano.garay@gmail.com>
2016-10-27 15:19:01 -0400

    refactorized and extracted players

---
+++ XAudio/html/AudioContextPlayer.js
@@ -0,0 +1,19 @@
+﻿function AudioContextPlayer(buffer) {
+
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var self = this;
+    var context = new AudioContext();
+
+    var playWithAudioContext = function (buffer) {
+        var source = context.createBufferSource(); // creates a sound source
+        //var rightChannel = buffer.getChannelData(0); //encoded on Float32
+        //var left = buffer.getChannelData(1);
+        source.buffer = buffer; // tell the source which sound to play
+        source.connect(context.destination); // connect the source to the context's destination (the speakers)
+        source.start(0); // play the source now
+    };
+
+    self.play = function () {
+        context.decodeAudioData(buffer, playWithAudioContext, function (e) { console.error(e); });
+    };
+}
---
+++ XAudio/html/XAudioPlayer.js
@@ -0,0 +1,65 @@
+﻿function XAudioPlayer(buffer) {
+    
+    var self = this;
+    var encodedBufferOffset = 0;
+    var reachedEndOfBuffer = false;
+    var wav = new WaveParser(buffer);
+    wav.ReadHeader();
+    var encodedBuffer = buffer.slice(wav.GetHeaderSize(), buffer.byteLength);
+    var gsmDecoder = new GsmDecoder();
+
+    var failureCallback = function(e) {console.error("XAudioPlayer: " + e);}
+
+    var getSamplesCallback = function (samplesRequested) {
+        if (wav.format.formatID == 'gsm')
+            return gsmDecoder.decode(self.getEncodedBlocks());
+        else //assuming it is lpcm
+            return self.getPcmBlocks(samplesRequested);
+    };
+    
+    self.getEncodedBlocks = function () {
+        var encodedBlock = encodedBuffer.slice(encodedBufferOffset, encodedBufferOffset + (65 * 10));
+        
+        encodedBufferOffset += encodedBlock.length;
+        reachedEndOfBuffer = encodedBufferOffset >= encodedBuffer.length;
+        return encodedBlock;
+    };
+    
+    self.getPcmBlocks = function (samplesRequested) {
+        //-1 ONLY FOR SIGNED PCM WAVE that said for bitsPerAample above 16bit. 8-bit format are always signed pcm waves
+        var conversionFact = Math.pow(2, wav.format.significantBitsPerSample - (wav.format.significantBitsPerSample == 8 ? 0 : 1));
+        
+        var bitsPerSampleArray = wav.format.significantBitsPerSample == 8 ? new Uint8Array(encodedBuffer.buffer, encodedBufferOffset) : new Int16Array(encodedBuffer.buffer, encodedBufferOffset);
+        // Each sample is the bitsPerSample per the channels
+        var decodedFloat = new Float32Array(samplesRequested * wav.format.channelsPerFrame /*3200 * 6*/);
+        
+        for (var i = 0; i < decodedFloat.length; i++) {
+            decodedFloat[i] = bitsPerSampleArray[i] / conversionFact;
+            encodedBufferOffset += bitsPerSampleArray.BYTES_PER_ELEMENT;
+        }
+        
+        if (encodedBufferOffset >= encodedBuffer.length)
+            reachedEndOfBuffer = true;
+        return decodedFloat;
+    };
+
+    self.play = function() {
+       var intervalId = setInterval(function () {
+            if (reachedEndOfBuffer) {
+                clearInterval(intervalId);
+                return;
+            }
+            
+            xAudioServer.executeCallback();
+        }, 200);
+    };
+
+    var xAudioServer = new XAudioServer(
+        wav.format.channelsPerFrame,
+            wav.format.sampleRate,
+            wav.format.sampleRate / 4,
+            wav.format.sampleRate / 2,
+            getSamplesCallback,
+            1,
+            failureCallback);
+}
