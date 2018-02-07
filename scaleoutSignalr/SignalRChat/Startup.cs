54c5b43ee73237c2f7bc4b8c9aada7b00371989a
Marco Medrano <marco.medrano.garay@gmail.com>
2018-02-07 17:52:50 +0000

    Working only inside AWS

--- scaleoutSignalr/SignalRChat/Startup.cs
+++ scaleoutSignalr/SignalRChat/Startup.cs
@@ -9,7 +9,9 @@ namespace SignalRChat
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            GlobalHost.DependencyResolver.UseRedis("redisServer", 6379, "password", "ChatApp");
+            //GlobalHost.DependencyResolver.UseRedis("ec2-34-238-42-75.compute-1.amazonaws.com", 6379, "connect77", "ChatApp");
+            //GlobalHost.DependencyResolver.UseRedis("sosmarco.astutesolutions.org", 6379, "connect77", "ChatApp");
+            GlobalHost.DependencyResolver.UseRedis("epcqasosredis.7scqkw.ng.0001.use1.cache.amazonaws.com", 6379, string.Empty, "ChatApp");
             app.MapSignalR();
         }
     }
